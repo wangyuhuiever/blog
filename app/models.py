3d57da7e5c8e708355ffe0ab46100eaa9222f892
wangyuhuiever <wangyuhuiever@163.com>
2017-06-12 22:22:08 +0800

    增加了API接口。

--- app/models.py
+++ app/models.py
@@ -1,12 +1,13 @@
 from . import db, login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask import current_app, request
+from flask import current_app, request, url_for
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask_login import UserMixin, AnonymousUserMixin
 from datetime import datetime
 import hashlib
 import bleach
 from markdown import markdown
+from app.exceptions import ValidationError
 
 class Follow(db.Model):
     __tablename__ = 'follows'
@@ -188,6 +189,33 @@ class User(UserMixin, db.Model):
         return Post.query.join(Follow, Follow.followed_id == Post.author_id) \
     .filter(Follow.follower_id == self.id)
 
+    def generate_auth_token(self, expiration):
+        s = Serializer(current_app.config['SECRET_KEY'],
+                       expires_in=expiration)
+        return s.dumps({'id': self.id})
+
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return None
+        return User.query.get(data['id'])
+
+    def to_json(self):
+        json_user = {
+            'url': url_for('api.get_post', id=self.id, _external=True),
+            'username': self.username,
+            'member_since': self.member_since,
+            'last_seen': self.last_seen,
+            'posts': url_for('api.get_user_posts', id-self.id, _external=True),
+            'followed_posts': url_for('api.get_user_followed_posts',
+                                      id=self.id, _external=True),
+            'post_count': self.posts.count()
+        }
+        return json_user
+
     def __repr__(self):
         return '<User %r>' % self.username
 
@@ -274,6 +302,26 @@ class Post(db.Model):
                      author=u)
             db.session.add(p)
             db.session.commit()
+    def to_json(self):
+        json_post = {
+            'url': url_for('api.get_post', id = self.id, _external=True),
+            'body': self.body,
+            'body_html': self.body_html,
+            'timestamp': self.timestamp,
+            'author': url_for('api.get_user', id = self.author_id,
+                              _external=True),
+            'comments': url_for('api.get_post_comments', id = self.id,
+                                 _external=True),
+            'comments_count': self.comments.count()
+        }
+        return json_post
+
+    @staticmethod
+    def from_json(json_post):
+        body = json_post.get('body')
+        if body is None or body == '':
+            raise ValidationError('文章没有内容')
+        return Post(body=body)
 
 db.event.listen(Post.body, 'set', Post.on_changed_body)
 
@@ -294,5 +342,23 @@ class Comment(db.Model):
         target.body_html = bleach.linkify(bleach.clean(
             markdown(value, output_format='html'),
             tags=allowed_tags, strip=True))
+
+    def to_json(self):
+        json_comment = {
+            'url': url_for('api.get_comment', id=self.id, _external=True),
+            'post': url_for('api.get_post', id=self.id, _external=True),
+            'body': self.body,
+            'body_html': self.body_html,
+            'timestamp': self.timestamp,
+            'author': url_for('api.get_user', id=self.author_id, _external=True)
+        }
+        return json_comment
+
+    @staticmethod
+    def from_json(json_comment):
+        body = json_comment.get('body')
+        if body is None or body == '':
+            raise ValidationError('评论没有内容')
+        return Comment(body=body)
 
 db.event.listen(Comment.body, 'set', Comment.on_change_body)
