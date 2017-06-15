752c8f7cce5e03993c2cecbcf3a4f5eba297154f
wangyuhuiever <wangyuhuiever@163.com>
2017-06-15 11:26:43 +0800

    被系统退信说垃圾邮件？配置gmail邮箱。

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_RECORD_QUERIES = True
     BLOG_ADMIN = '729265425@qq.com'
-    MAIL_SERVER = 'smtp.163.com'
-    MAIL_PORT = 25
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
